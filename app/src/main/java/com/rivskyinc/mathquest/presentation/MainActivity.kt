d311cf4b0fd6c257bee286b9cf852986fa6016bf
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-12 13:15:08 +0300

    Initial commit

---
+++ app/src/main/java/com/rivskyinc/mathquest/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rivskyinc.mathquest.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rivskyinc.mathquest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

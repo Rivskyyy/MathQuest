bcc47be60828371c3173f52b233d1f5bb86c6939
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-26 02:39:59 +0300

    added GameFragment

--- app/src/main/java/com/rivskyinc/mathquest/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/rivskyinc/mathquest/presentation/ChooseLevelFragment.kt
@@ -12,7 +12,6 @@ class ChooseLevelFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_choose_level, container, false)
     }
 }
--- app/src/main/java/com/rivskyinc/mathquest/presentation/GameFragment.kt
+++ app/src/main/java/com/rivskyinc/mathquest/presentation/GameFragment.kt
@@ -7,13 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.rivskyinc.mathquest.R
 
-
-class WelcomeFragment : Fragment() {
+class GameFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        return inflater.inflate(R.layout.fragment_game, container, false)
     }
-
 }

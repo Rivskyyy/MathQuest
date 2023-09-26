c3c8e86105a3f57147574042641924cf32d4d481
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-26 19:44:01 +0300

    added launchChooseLevelFragment() to WelcomeFragment.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.rivskyinc.mathquest"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.rivskyinc.mathquest"
--- app/src/main/java/com/rivskyinc/mathquest/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/rivskyinc/mathquest/presentation/WelcomeFragment.kt
@@ -5,21 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.rivskyinc.mathquest.R
 import com.rivskyinc.mathquest.databinding.FragmentWelcomeBinding
 
 
 class WelcomeFragment : Fragment() {
-    private  var _binding : FragmentWelcomeBinding? = null
+    private var _binding: FragmentWelcomeBinding? = null
 
-    private val binding : FragmentWelcomeBinding
+    private val binding: FragmentWelcomeBinding
         get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentWelcomeBinding.inflate(inflater, container, false )
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnWelcomeAccept.setOnClickListener {
+            launchChooseLevelFragment()
+        }
+
+    }
+
+    private fun launchChooseLevelFragment() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, ChooseLevelFragment.newInstance())
+            .addToBackStack(null)
+            .commit()
     }
 
     override fun onDestroyView() {

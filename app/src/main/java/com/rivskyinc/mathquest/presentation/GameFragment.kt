4947cd1f74056a587da52b0aac2a64a8d8cf67b3
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-26 20:38:08 +0300

    added newInstance() to GameFragment.kt

--- app/src/main/java/com/rivskyinc/mathquest/presentation/GameFragment.kt
+++ app/src/main/java/com/rivskyinc/mathquest/presentation/GameFragment.kt
@@ -6,11 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.rivskyinc.mathquest.databinding.FragmentGameBinding
+import com.rivskyinc.mathquest.domain.entity.Level
 
 class GameFragment : Fragment() {
+    private lateinit var level : Level
     private var _binding : FragmentGameBinding? = null
     private val binding : FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null ")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,6 +27,20 @@ class GameFragment : Fragment() {
         return binding.root
     }
 
+    private fun parseArgs(){
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+    companion object{
+        private const val KEY_LEVEL = "level"
+        fun newInstance(level : Level) : GameFragment{
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+
+            }
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null

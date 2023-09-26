e6230fa6c903c307c63aef645ec4deb2ec87dc60
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-26 18:04:12 +0300

    added viewBinding to ChooseLevelFragment.kt

--- app/src/main/java/com/rivskyinc/mathquest/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/rivskyinc/mathquest/presentation/ChooseLevelFragment.kt
@@ -1,17 +1,27 @@
 package com.rivskyinc.mathquest.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.rivskyinc.mathquest.R
+import androidx.fragment.app.Fragment
+import com.rivskyinc.mathquest.databinding.FragmentChooseLevelBinding
+
 class ChooseLevelFragment : Fragment() {
+    private var _binding : FragmentChooseLevelBinding? = null
+    private val binding : FragmentChooseLevelBinding
+        get() = _binding ?: throw  RuntimeException("FragmentChooseLevelBinding == null ")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false )
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

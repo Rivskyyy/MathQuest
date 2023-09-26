ef132ec47004bb92ea4d4c31ea12f9aa8295a366
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-26 23:50:19 +0300

    adding newInstance to GameFinishFragment.kt

--- app/src/main/java/com/rivskyinc/mathquest/domain/entity/GameSettings.kt
+++ app/src/main/java/com/rivskyinc/mathquest/domain/entity/GameSettings.kt
@@ -1,11 +1,13 @@
 package com.rivskyinc.mathquest.domain.entity
 
+import java.io.Serializable
+
 data class GameSettings(
     val maxSumValue: Int,
     val minCountOfRightAnswers : Int,
     val minPercentOfRightAnswers : Int,
     val gameTimeInSeconds : Int
-)
+) : Serializable
 
 
 
--- app/src/main/java/com/rivskyinc/mathquest/presentation/GameFinishFragment.kt
+++ app/src/main/java/com/rivskyinc/mathquest/presentation/GameFinishFragment.kt
@@ -6,17 +6,38 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.rivskyinc.mathquest.databinding.FragmentGameFinishBinding
+import com.rivskyinc.mathquest.domain.entity.GameResult
 
 class GameFinishFragment : Fragment() {
+    private lateinit var gameResult: GameResult
     private var _binding : FragmentGameFinishBinding? = null
     private val binding : FragmentGameFinishBinding
         get() = _binding ?: throw RuntimeException("FragmentGameFinishBinding == null ")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGameFinishBinding.inflate(inflater, container, false )
         return binding.root
+    }
+    private fun parseArgs(){
+        gameResult = requireArguments().getSerializable(KEY_GAME_RESULT) as GameResult
+    }
+    companion object{
+
+        private const val KEY_GAME_RESULT = "game_result"
+
+        fun newInstance(gameResult : GameResult) : GameFinishFragment {
+            return GameFinishFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_GAME_RESULT, gameResult)
+                }
+            }
+        }
     }
     override fun onDestroyView() {
         super.onDestroyView()

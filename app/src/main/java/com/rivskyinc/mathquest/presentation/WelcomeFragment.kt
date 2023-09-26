57cfb55f5b44611200733fcc903c6c814f2e30e6
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-26 18:03:35 +0300

    added viewBinding to WelcomeFragment.kt

--- app/src/main/java/com/rivskyinc/mathquest/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/rivskyinc/mathquest/presentation/WelcomeFragment.kt
@@ -1,19 +1,29 @@
 package com.rivskyinc.mathquest.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.rivskyinc.mathquest.R
+import androidx.fragment.app.Fragment
+import com.rivskyinc.mathquest.databinding.FragmentWelcomeBinding
 
 
 class WelcomeFragment : Fragment() {
+    private  var _binding : FragmentWelcomeBinding? = null
+
+    private val binding : FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false )
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }

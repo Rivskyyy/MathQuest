e3621d92736e1017af8dc6b5746ea6607e78dbd3
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-27 13:40:27 +0300

    Serializable modified to Parcelable

--- app/src/main/java/com/rivskyinc/mathquest/domain/entity/GameSettings.kt
+++ app/src/main/java/com/rivskyinc/mathquest/domain/entity/GameSettings.kt
@@ -1,13 +1,15 @@
 package com.rivskyinc.mathquest.domain.entity
 
-import java.io.Serializable
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class GameSettings(
     val maxSumValue: Int,
     val minCountOfRightAnswers : Int,
     val minPercentOfRightAnswers : Int,
     val gameTimeInSeconds : Int
-) : Serializable
+) : Parcelable
 
 
 
